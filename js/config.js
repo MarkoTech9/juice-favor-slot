3aef468a8c5b83bc4e4105653a0518489b9260c4
MarkoTech9 <fauvellemarc@gmail.com>
2026-08-01 03:28:02 -0400

    Create config.js

---
+++ js/config.js
@@ -0,0 +1,44 @@
+const GAME_CONFIG = {
+
+    startingCredits:1000,
+
+    rows:4,
+    reels:5,
+
+    defaultBet:20,
+
+    freeSpinCount:10,
+
+
+    symbols:{
+
+        low:[
+            "🍋",
+            "🍒",
+            "🍊"
+        ],
+
+        medium:[
+            "🍉",
+            "🍍"
+        ],
+
+        high:[
+            "💎",
+            "⭐"
+        ],
+
+
+        wild:[
+            "WILDx2",
+            "WILDx3"
+        ],
+
+
+        scatter:[
+            "SCATTER"
+        ]
+
+    }
+
+};
