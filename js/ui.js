ad048d485fcb7d8f83b21bbbaaa29f98bc63b3fd
MarkoTech9 <fauvellemarc@gmail.com>
2026-08-01 03:33:24 -0400

    Update ui.js

--- js/ui.js
+++ js/ui.js
@@ -1,1 +1,62 @@
+function renderBoard(board){
 
+
+    const slot =
+    document.getElementById("slot");
+
+
+    slot.innerHTML="";
+
+
+    board.forEach(row=>{
+
+
+        row.forEach(symbol=>{
+
+
+            let cell =
+            document.createElement("div");
+
+
+            cell.className="symbol";
+
+
+            cell.innerHTML=symbol;
+
+
+            slot.appendChild(cell);
+
+
+        });
+
+
+    });
+
+
+}
+
+
+
+
+
+function showMessage(text){
+
+
+    document.getElementById("message")
+    .innerHTML=text;
+
+
+}
+
+
+
+
+
+function updateCredits(value){
+
+
+    document.getElementById("credits")
+    .innerHTML=value;
+
+
+}
